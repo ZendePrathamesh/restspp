5d4b6333921455741bb54502b0c043557d6bbb1b
Shivani99raskar <shivaniraskar99@gmail.com>
2024-09-18 16:02:58 +0530

    Product Management Code

--- src/main/java/com/fq/restspp/enitity/Customer.java
+++ src/main/java/com/fq/restspp/enitity/Customer.java
@@ -33,7 +33,7 @@ public class Customer {
   @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
   private String name;
 
-  @Email(message = "Email should be valid")
+  @Email(message = "Email should be valid.")
   @NotBlank(message = "Email is required")
   private String email;
 
